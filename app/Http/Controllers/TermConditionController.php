89013a0d8cabdc5a3204cf7f2fa27049114f8a7e
AbacusIntlere <abacusantlere@gmail.com>
2023-03-03 15:21:32 +0500

    First Commit

---
+++ app/Http/Controllers/TermConditionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TermCondition;
+use App\Http\Requests\StoreTermConditionRequest;
+use App\Http\Requests\UpdateTermConditionRequest;
+
+class TermConditionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTermConditionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TermCondition $termCondition)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TermCondition $termCondition)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTermConditionRequest $request, TermCondition $termCondition)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TermCondition $termCondition)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\AboutUsComponent;
+use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\ThankYouComponent;
+use App\Http\Livewire\ContactUsComponent;
+use App\Http\Livewire\PrivacyPolicyComponent;
+use App\Http\Livewire\ReturnPolicyComponent;
+use App\Http\Livewire\TermsConditionsComponent;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Home Component
+Route::get('/', HomeComponent::class)->name('home');
+// Shop Component
+Route::get('shop', ShopComponent::class)->name('shop');
+// Cart Component
+Route::get('cart', CartComponent::class)->name('cart');
+// Check Out Component
+Route::get('checkout', CheckoutComponent::class)->name('checkout');
+// Thank You Route Component
+Route::get('/thank-you', ThankYouComponent::class)->name('thank.you');
+// About Us Component
+Route::get('about-us', AboutUsComponent::class)->name('about.us');
+// Contact Us Component
+Route::get('contact-us', ContactUsComponent::class)->name('contact.us');
+// Terms & Conditions Component
+Route::get('terms-conditions', TermsConditionsComponent::class)->name('terms.conditions');
+// Return Policy Component
+Route::get('return-policy', ReturnPolicyComponent::class)->name('return.policy');
+// Privacy and Policy Component
+Route::get('privacy-policy', PrivacyPolicyComponent::class)->name('privacy.policy');
+
+
+
+
+// Authentication Routes
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
