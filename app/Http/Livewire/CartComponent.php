b14fb84a9935f9bc2032074015adeed4c0bdb7af
AbacusIntlere <abacusantlere@gmail.com>
2023-03-13 13:48:39 +0500

    Cart Updates

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,11 +3,42 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Gloudemans\Shoppingcart\Facades\Cart;
 
 class CartComponent extends Component
 {
     public function render()
     {
         return view('livewire.cart-component')->layout('layouts.base');
+    }
+    // For Increasing Quantity Into Cart
+    public function increaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty+1;
+        Cart::update($rowId, $qty);
+    }
+
+    // For Decreasing Quantity Into Cart
+    public function decreaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty-1;
+        Cart::update($rowId, $qty);
+    }
+
+    // For Deleting Product From Cart
+    public function remove($rowId)
+    {
+        Cart::remove($rowId);
+        session()->flash('success_message', "Product Deleted From Cart");
+    }
+
+    // For Deleting All Products From Cart
+    public function removeAll()
+    {
+        Cart::destroy();
+        session()->flash('success_message', "Shopping Cart Cleared Successfully!");
+
     }
 }
--- resources/views/livewire/details-component.blade.php
+++ resources/views/livewire/details-component.blade.php
@@ -58,20 +58,16 @@
                         <i class="fa fa-star" aria-hidden="true"></i>
                         <a href="#" class="count-review">(05 review)</a>
                     </div>
-                    <h2 class="product-name">Radiant-360 R6 Wireless Omnidirectional Speaker [White]</h2>
+                    <h2 class="product-name">{{ $product->name }}</h2>
                     <div class="short-desc">
-                        <ul>
-                            <li>7,9-inch LED-backlit, 130Gb</li>
-                            <li>Dual-core A7 with quad-core graphics</li>
-                            <li>FaceTime HD Camera 7.0 MP Photos</li>
-                        </ul>
+                        {{ $product->short_desc }}
                     </div>
                     <div class="wrap-social">
                         <a class="link-socail" href="#"><img src="assets/images/social-list.png" alt=""></a>
                     </div>
-                    <div class="wrap-price"><span class="product-price">$250.00</span></div>
+                    <div class="wrap-price"><span class="product-price">${{ $product->regular_price }}</span></div>
                     <div class="stock-info in-stock">
-                        <p class="availability">Availability: <b>In Stock</b></p>
+                        <p class="availability">Availability: <b>{{ $product->stock_status }}</b></p>
                     </div>
                     <div class="quantity">
                         <span>Quantity:</span>
@@ -83,7 +79,7 @@
                         </div>
                     </div>
                     <div class="wrap-butons">
-                        <a href="#" class="btn add-to-cart">Add to Cart</a>
+                        <a href="#" class="btn add-to-cart" wire:click.prevent="addToCart({{ $product->id }},{{ $product->name }}, {{ $product->regular_price }})">Add to Cart</a>
                         <div class="wrap-btn">
                             <a href="#" class="btn btn-compare">Add Compare</a>
                             <a href="#" class="btn btn-wishlist">Add Wishlist</a>
@@ -98,16 +94,7 @@
                     </div>
                     <div class="tab-contents">
                         <div class="tab-content-item active" id="description">
-                            <p>Lorem ipsum dolor sit amet, an munere tibique consequat mel, congue albucius no qui, a t
-                                everti meliore erroribus sea. ro cum. Sea ne accusata voluptatibus. Ne cum falli dolor
-                                voluptua, duo ei sonet choro facilisis, labores officiis torquatos cum ei.</p>
-                            <p>Cum altera mandamus in, mea verear disputationi et. Vel regione discere ut, legere
-                                expetenda ut eos. In nam nibh invenire similique. Atqui mollis ea his, ius graecis
-                                accommodare te. No eam tota nostrum eque. Est cu nibh clita. Sed an nominavi, et
-                                stituto, duo id rebum lucilius. Te eam iisque deseruisse, ipsum euismod his at. Eu
-                                putent habemus voluptua sit, sit cu rationibus scripserit, modus taria . </p>
-                            <p>experian soleat maluisset per. Has eu idque similique, et blandit scriptorem tatibus mea.
-                                Vis quaeque ocurreret ea.cu bus scripserit, modus voluptaria ex per.</p>
+                            {{ $product->description }}
                         </div>
                         <div class="tab-content-item " id="add_infomation">
                             <table class="shop_attributes">
