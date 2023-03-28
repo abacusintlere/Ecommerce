426540f1e943173d841b11736b573946d7058404
AbacusIntlere <abacusantlere@gmail.com>
2023-03-28 11:25:52 +0500

    Checkout Procedure Completed

--- app/Http/Livewire/CheckoutComponent.php
+++ app/Http/Livewire/CheckoutComponent.php
@@ -2,13 +2,144 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Order;
 use Livewire\Component;
+use App\Models\Shipping;
+use App\Models\OrderItem;
+use App\Models\Transaction;
+use Illuminate\Support\Facades\Auth;
+use Gloudemans\Shoppingcart\Facades\Cart;
 
 class CheckoutComponent extends Component
 {
-    public $ship_to_different, $firstname, $lastname, $email, $mobile, $line1, $line2, $city, $province, $country, $zipcode, $s_firstname, $s_lastname, $s_email, $s_mobile, $s_line1, $s_line2, $s_city, $s_province, $s_country, $s_zipcode;
+    public $ship_to_different, $firstname, $lastname, $email, $mobile, $line1, $line2, $city, $province, $country, $zipcode, $s_firstname, $s_lastname, $s_email, $s_mobile, $s_line1, $s_line2, $s_city, $s_province, $s_country, $s_zipcode, $paymentmode;
     public function render()
     {
         return view('livewire.checkout-component')->layout('layouts.base');
+    }
+
+    // Hook For Live Validation
+    public function updated($fields)
+    {
+        $this->validateOnly($fields, [
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'required|email',
+            'mobile' => 'required|numeric',
+            'line1' => 'required',
+            'city' => 'required',
+            'province' => 'required',
+            'country' => 'required',
+            'zipcode' => 'required',
+            'paymentmode' => 'required',
+        ]);
+
+        // Validate Shipping Fields Only If Shipping Address is Different from Billing
+        if($this->ship_to_different)
+        {
+            $this->validateOnly($fields, [
+                's_firstname' => 'required',
+                's_lastname' => 'required',
+                's_email' => 'required|email',
+                's_mobile' => 'required|numeric',
+                's_line1' => 'required',
+                's_city' => 'required',
+                's_province' => 'required',
+                's_country' => 'required',
+                's_zipcode' => 'required',
+            ]);
+        }
+    }
+
+    // For Placing Order
+    public function placeOrder()
+    {
+        $this->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'required|email',
+            'mobile' => 'required|numeric',
+            'line1' => 'required',
+            'city' => 'required',
+            'province' => 'required',
+            'country' => 'required',
+            'zipcode' => 'required',
+            'paymentmode' => 'required',
+        ]);
+
+        $order = new Order();
+        $order->user_id = Auth::user()->id;
+        $order->subtotal = session()->get('checkout')['subtotal'];
+        $order->discount = session()->get('checkout')['discount'];
+        $order->tax = session()->get('checkout')['tax'];        
+        $order->total = session()->get('checkout')['total'];
+        $order->firstname = $this->firstname;
+        $order->lastname = $this->lastname;
+        $order->mobile = $this->mobile;
+        $order->email = $this->email;
+        $order->line1 = $this->line1;
+        $order->line2 = $this->line2;
+        $order->city = $this->city;
+        $order->province = $this->province;
+        $order->country = $this->country;
+        $order->zipcode = $this->zipcode;
+        $order->status = 'ordered';
+        $order->is_shipping_different = $this->ship_to_different ? 1 : 0;
+        $order->save();
+
+        // Saving Order Items
+        foreach(Cart::instance('cart')->content() as $item)
+        {
+            $orderItem = new OrderItem();
+            $orderItem->order_id = $order->id;
+            $orderItem->product_id = $item->id;
+            $orderItem->price = $item->price;
+            $orderItem->quantity = $item->qty;
+            $orderItem->save();
+
+        }
+
+        if($this->is_shipping_different)
+        {
+            $this->validate([
+                's_firstname' => 'required',
+                's_lastname' => 'required',
+                's_email' => 'required|email',
+                's_mobile' => 'required|numeric',
+                's_line1' => 'required',
+                's_city' => 'required',
+                's_province' => 'required',
+                's_country' => 'required',
+                's_zipcode' => 'required',
+            ]);
+
+            $shipping = new Shipping();
+            $shipping->order_id = $order->id;
+            $shipping->firstname = $this->s_firstname;
+            $shipping->lastname = $this->s_lastname;
+            $shipping->mobile = $this->s_mobile;
+            $shipping->email = $this->s_email;
+            $shipping->line1 = $this->s_line1;
+            $shipping->line2 = $this->s_line2;
+            $shipping->city = $this->s_city;
+            $shipping->province = $this->s_province;
+            $shipping->country = $this->s_country;
+            $shipping->zipcode = $this->s_zipcode;
+            $shipping->save();
+        }
+
+        if($this->paymentmode)
+        {
+            $transaction  = new Transaction();
+            $transaction->user_id = Auth::user()->id;
+            $transaction->order_id = $order->id;
+            $transaction->mode = "cod";
+            $transaction->status = "pending";
+            $transaction->save();
+
+        }
+        // Now Finally Destroying Cart
+        Cart::instance('cart')->destroy();
+        session()->forget('checkout');
     }
 }
